e0d03243243c8edf892abd0573cb86fd9dc6b2d9
Christian <cvernikoff@gmail.com>
2019-02-09 13:05:22 -0800

    some styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,10 +5,19 @@ class Sidebar extends Component {
     render() {
         return (
             <div className="col sidebar">
-                <ul className="list-group">
-                    <li className="sidebar-item list-group-item border-0">Home</li>
-                    <li className="sidebar-item list-group-item border-0">Trending</li>
-                    <li className="sidebar-item list-group-item border-0">Random</li>
+                <ul className="list-group list-group-flush">
+                    <li className="sidebar-item list-group-item border-0 d-flex align-items-center flex-row">
+                        <i className="fas fa-home"/>
+                        <span className="sidebar-icon-text">Home</span>
+                    </li>
+                    <li className="sidebar-item list-group-item border-0 d-flex align-items-center flex-row">
+                        <i className="fas fa-fire"/>
+                        <span className="sidebar-icon-text">Trending</span>
+                    </li>
+                    <li className="sidebar-item list-group-item border-0 d-flex align-items-center flex-row">
+                        <i className="fas fa-random"/>
+                        <span className="sidebar-icon-text">Random</span>
+                    </li>
                 </ul>
             </div>
         );
